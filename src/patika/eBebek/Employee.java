447d8e9810c4a461e7c09246cb0abc3eb26f89ff
Hanife Hazırlar <hanifehzrlar@gmail.com>
2022-10-09 00:06:16 +0300

    09/10/22

---
+++ src/patika/eBebek/Employee.java
@@ -0,0 +1,78 @@
+package patika.eBebek;
+
+public class Employee {
+   String name;
+   double salary;
+   int workHours;
+   int hireYear;
+
+    public Employee(String name, double salary, int workHours, int hireYear) {
+        this.name = name;
+        this.salary = salary;
+        this.workHours = workHours;
+        this.hireYear = hireYear;
+    }
+
+    public Employee() {
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", workHours=" + workHours +
+                ", hireYear=" + hireYear +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Fatma Mavi",2500,45,2010);
+        System.out.println(employee);
+        System.out.println("Tax : "+tax(employee.salary)+ " $");
+        System.out.println("Bonus : "+bonus(employee.workHours)+" $");
+        System.out.println("Raise salary : "+raiseSalary(employee.hireYear,employee.salary)+" $");
+        System.out.println("Salary with bonuses : "+(employee.salary+bonus(employee.workHours))+" $");
+        System.out.println("Total Salary : "+(employee.salary+bonus(employee.workHours)+
+                raiseSalary(employee.hireYear,employee.salary)-tax(employee.salary))+" $");
+    }
+
+    private static double raiseSalary(int hireYear,double salary) {
+        double raiseSalary=0;
+        if ((2021-hireYear)<10){
+            raiseSalary=salary*0.05;
+        }else if((2021-hireYear>9 & (2021-hireYear)<20)){
+            raiseSalary=salary*0.10;
+        }else{
+            System.out.println("Isyerimizde 20 ve uzeri yil calisilamaz");
+        }
+        return raiseSalary;
+    }
+
+    private static double bonus(int workHours) {
+        double bonus=0;
+        if (workHours>40){
+            bonus=(workHours-40)*30;
+        }else{
+            bonus= 0;
+        }
+        return bonus;
+    }
+
+    private static double tax(double salary) {
+        double tax=0;
+        if (salary<1000){
+            tax=0;
+        }else{
+            tax=salary*0.03;
+        }
+        return tax;
+    }
+
+
+
+
+
+
+}
